8a19fcc26e645925afafd1af6b3cbcff195b8065
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-10 21:47:21 +0700

    add refresh token func

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,9 +2,22 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./components/sidebar";
 import { useSelector } from "react-redux";
 import Header from "./components/header";
+import { useEffect } from "react";
+import { apiRefreshToken } from "./api/endpoint/auth";
+import store from "./store/store";
 
 const Layout = () => {
   const ui = useSelector((state) => state.ui.value);
+
+  const refreshToken = async () => {
+    await apiRefreshToken(store.getState().auth.token);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      refreshToken();
+    }, 6000000);
+  });
   return (
     <div className="min-h-[500px] relative">
       <Sidebar />
