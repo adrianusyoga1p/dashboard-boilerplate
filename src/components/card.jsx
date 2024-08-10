c32166d96f146a5bd3294ec98d7c8dad37d1b98d
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-10 21:48:21 +0700

    modify card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,26 @@
+import { useFormatter } from "@/utils/useFormatter";
 import { ModalButton } from "./modal";
 
-const Card = ({ title, subtitle, children, action = false }) => {
+const Card = ({ title, subtitle, children, action = false, className }) => {
+  const { cn } = useFormatter();
   return (
-    <div className="rounded-xl shadow-md bg-white p-6">
-      <div className="flex items-center justify-between border-b border-gray-200 mb-4">
-        <div className="space-y-1.5">
-          <h2 className="text-gray-800 text-lg font-semibold">{title}</h2>
-          <p className="text-gray-400 pb-2 text-sm">{subtitle}</p>
+    <div className={cn("rounded-xl shadow-md bg-white p-6", className)}>
+      {title && (
+        <div className="flex items-center justify-between border-b border-gray-200 mb-4">
+          <div className="space-y-1.5">
+            <h2 className="text-gray-800 text-lg font-semibold">{title}</h2>
+            <p className="text-gray-400 pb-2 text-sm">{subtitle}</p>
+          </div>
+          {action && (
+            <ModalButton color="dark" size="md" id="addData">
+              Add New
+            </ModalButton>
+          )}
         </div>
-        {action && <ModalButton color="dark" size="md" id="addData">Add New</ModalButton>}
-      </div>
-      <div>
-        {children}
-      </div>
+      )}
+      <div>{children}</div>
     </div>
   );
 };
-
 
 export default Card;
