d53c0c0b87b94242ed7edc0ff509d1870b6f610b
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-11 15:38:42 +0700

    add endpoint of users

--- src/api/endpoint/users.js
+++ src/api/endpoint/users.js
@@ -1,9 +1,25 @@
-import api from "../client"
+import api from "../client";
 
 export const apiUsers = (params) => {
   return api({
     method: "GET",
     url: "/users",
-    params
+    params,
+  });
+};
+
+export const apiUsersUpdate = (id, data) => {
+  return api({
+    method: "PUT",
+    url: "/users/" + id,
+    data,
+  });
+};
+
+export const apiUserPost = (data) => {
+  return api({
+    method: "POST",
+    url: "users/add",
+    data
   })
 }
