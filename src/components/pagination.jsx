d368e30a1fdc2bedbc1e0786751848dade87fee9
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-12 17:16:26 +0700

    add pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,40 +1,63 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-const Pagination = ({ total, limit, onPageChange }) => {
+const Pagination = ({ total, limit, onPageChange, page }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = Math.ceil(total / limit);
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-    const skip = (page - 1) * limit;
-    onPageChange(skip);
+  useEffect(() => {
+    if (page && page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  }, [page, totalPages]);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+      const skip = (newPage - 1) * limit;
+      onPageChange(skip);
+    }
   };
 
+  // Menentukan startPage dan endPage dengan batasan yang tepat
+  const startPage = Math.max(1, currentPage - 2);
+  const endPage = Math.min(totalPages, startPage + 4);
+
+  const pageRange = [];
+  for (let i = startPage; i <= endPage; i++) {
+    pageRange.push(i);
+  }
+
   return (
-    <div className='flex items-center'>
-      <button 
-        onClick={() => handlePageChange(currentPage - 1)} 
-        disabled={currentPage === 1}
-      >
-        Previous
-      </button>
-      
-      {Array.from({ length: totalPages }, (_, index) => (
+    <div className="flex items-center mt-4 gap-3 justify-center text-sm">
+      {currentPage > 1 && (
         <button
-          key={index + 1}
-          onClick={() => handlePageChange(index + 1)}
-          className={`${currentPage === index + 1 ? 'bg-sky-500 text-white' : ''} w-10 h-10 flex items-center justify-center`}
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
         >
-          {index + 1}
+          Previous
+        </button>
+      )}
+
+      {pageRange.map((number) => (
+        <button
+          key={number}
+          onClick={() => handlePageChange(number)}
+          className={`${
+            number === currentPage ? "bg-sky-700 text-white" : ""
+          } w-8 h-8 flex items-center justify-center rounded`}
+        >
+          {number}
         </button>
       ))}
 
-      <button 
-        onClick={() => handlePageChange(currentPage + 1)} 
-        disabled={currentPage === totalPages}
-      >
-        Next
-      </button>
+      {currentPage < totalPages && (
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      )}
     </div>
   );
 };
