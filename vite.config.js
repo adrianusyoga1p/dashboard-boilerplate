44b5d6d5a80f3aefb0608fd561dcd7536c709275
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-12 15:31:34 +0700

    adjust viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_API_URL,
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  },
+  // base: process.env.VITE_API_URL,
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
