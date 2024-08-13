0cd12895c0e60863ccef3cbaf64d4ef4b1dad7c4
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-13 17:07:30 +0700

    adjust component btn and input

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,44 +1,46 @@
-const BaseButton = ({ size = "sm", color = "blue", children, onClick, type }) => {
-  const sizeButton = () => {
-    if (size == "sm") {
-      return "text-xs py-1.5 px-3 rounded";
-    } else if (size == "md") {
-      return "py-2 text-sm px-4 rounded-md";
-    } else if (size == "lg") {
-      return "py-3 px-6 rounded-lg text-lg";
-    } else if (size == "xl") {
-      return "py-4 px-8 rounded-lg text-xl";
-    } else {
-      return (size = "md");
-    }
-  };
+import { forwardRef } from "react";
 
-  const colorButton = () => {
-    if (color == "blue") {
-      return "bg-sky-600 text-white";
-    } else if (color == "yellow") {
-      return "bg-yellow-500 text-white";
-    } else if (color == "red") {
-      return "bg-red-500 text-white";
-    } else if (color == "green") {
-      return "bg-green-500 text-white";
-    } else if (color == "dark" || color == "black") {
-      return "bg-gray-800 text-white";
-    } else if (color == "light" || color == "white") {
-      return "bg-gray-100 text-gray-800";
-    } else {
-      return (color = "blue");
-    }
-  };
-  return (
-    <button
-      onClick={onClick}
-      type={type}
-      className={`font-medium ${sizeButton()} ${colorButton()}`}
-    >
-      {children}
-    </button>
-  );
-};
+const BaseButton = forwardRef(
+  ({ children, size = "md", color = "blue", ...props }, ref) => {
+    const sizeButton = () => {
+      if (size == "sm") {
+        return "text-xs py-1.5 px-3 rounded";
+      } else if (size == "md") {
+        return "py-2 text-sm px-4 rounded-md";
+      } else if (size == "lg") {
+        return "py-3 px-6 rounded-lg text-lg";
+      } else if (size == "xl") {
+        return "py-4 px-8 rounded-lg text-xl";
+      }
+    };
+
+    const colorButton = () => {
+      if (color == "blue") {
+        return "bg-sky-600 text-white";
+      } else if (color == "yellow") {
+        return "bg-yellow-500 text-white";
+      } else if (color == "red") {
+        return "bg-red-500 text-white";
+      } else if (color == "green") {
+        return "bg-green-500 text-white";
+      } else if (color == "dark" || color == "black") {
+        return "bg-gray-800 text-white";
+      } else if (color == "light" || color == "white") {
+        return "bg-gray-100 text-gray-800";
+      }
+    };
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className={`font-medium ${sizeButton()} ${colorButton()}`}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+BaseButton.displayName = "BaseButton";
 
 export default BaseButton;
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,19 +1,39 @@
-const BaseInput = ({ label, onChange, value, type = 'text', placeholder, name }) => {
+import { forwardRef } from "react";
+const BaseInput = forwardRef(({ label, ...props }, ref) => {
   return (
     <div>
       {label && (
         <label className="block mb-2 text-sm font-semibold">{label}</label>
       )}
       <input
-        type={type}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        name={name}
+        ref={ref}
+        {...props}
         className="w-full p-2 rounded-md border border-gray-300"
       />
     </div>
   );
-};
+});
+
+BaseInput.displayName = "BaseInput";
 
 export default BaseInput;
+
+// const BaseInput = ({ label, onChange, value, type = 'text', placeholder, name }) => {
+//   return (
+//     <div>
+//       {label && (
+//         <label className="block mb-2 text-sm font-semibold">{label}</label>
+//       )}
+//       <input
+//         type={type}
+//         value={value}
+//         onChange={onChange}
+//         placeholder={placeholder}
+//         name={name}
+//         className="w-full p-2 rounded-md border border-gray-300"
+//       />
+//     </div>
+//   );
+// };
+
+// export default BaseInput;
